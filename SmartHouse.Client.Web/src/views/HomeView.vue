93dc78729d59453218c886da7888d585a559ad6c
Yurij Boklan <hailrake@riseup.net>
2023-11-20 17:42:08 +0200

    update

--- SmartHouse.Client.Web/src/views/HomeView.vue
+++ SmartHouse.Client.Web/src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Hello world</h1>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {},
+});
+</script>
